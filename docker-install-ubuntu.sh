094d313551b3c18ae9ff492564f5d18879509709
MuratovAS <lexamzx@gmail.com>
2020-08-30 22:34:57 +0500

    add auto install

---
+++ docker-install-ubuntu.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#https://docs.docker.com/engine/install/ubuntu/
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository -y \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+
+echo "Restart computer and run: docker run hello-world"
